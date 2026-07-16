039a2a060fbeecc6e82e46df1c5e426f5d33cef6
saqshamshukla005-spec <saqshamshukla005@gmail.com>
2026-07-16 15:42:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,14 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  orderBy,
+  query
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5qCcW_pCATWTYQlXBAWfaSKnT3qzMROg",
@@ -8,11 +16,20 @@ const firebaseConfig = {
   projectId: "classhub-a2bf2",
   storageBucket: "classhub-a2bf2.firebasestorage.app",
   messagingSenderId: "788231446286",
-  appId: "1:788231446286:web:c6c0a6e27380c00f92815f",
-  measurementId: "G-7RMH8QSGXR"
+  appId: "1:788231446286:web:c6c0a6e27380c00f92815f"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 
-export { db };
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  orderBy,
+  query
+};
