22d7ad600a96b075a304a927e8ed0851ea6ad17f
saqshamshukla005-spec <saqshamshukla005@gmail.com>
2026-07-16 13:31:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("ClassHub Loaded Successfully!");
+document.addEventListener("DOMContentLoaded", function () {
 
     alert("Welcome to ClassHub 🎉");
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+
+        button.addEventListener("click", function () {
+
+            const text = this.innerText;
+
+            if (text.includes("Student")) {
+                alert("Student Login page will be available soon.");
+            }
+
+            else if (text.includes("Teacher")) {
+                alert("Teacher Login page will be available soon.");
+            }
+
+            else {
+                alert("CR-DC / Admin Login page will be available soon.");
+            }
+
+        });
+
+    });
+
 });
