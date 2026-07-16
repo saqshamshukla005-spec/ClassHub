5ef2ee755c4a5a0f7abab81531bf8809d7a9264f
saqshamshukla005-spec <saqshamshukla005@gmail.com>
2026-07-16 13:55:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("ClassHub Loaded Successfully");
 
     const studentBtn = document.querySelector("a[href='student-login.html']");
     const adminBtn = document.querySelector("a[href='admin-login.html']");
 
-    if(studentBtn){
-        studentBtn.addEventListener("click", function(){
+    if (studentBtn) {
+        studentBtn.onclick = function () {
             window.location.href = "student-login.html";
-        });
+        };
     }
 
-    if(adminBtn){
-        adminBtn.addEventListener("click", function(){
+    if (adminBtn) {
+        adminBtn.onclick = function () {
             window.location.href = "admin-login.html";
-        });
+        };
     }
 
 });
