1a9dede20db38959bd3a27afaf58a8aab6d68d7e
saqshamshukla005-spec <saqshamshukla005@gmail.com>
2026-07-16 14:58:59 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5qCcW_pCATWTYQlXBAWfaSKnT3qzMROg",
+  authDomain: "classhub-a2bf2.firebaseapp.com",
+  projectId: "classhub-a2bf2",
+  storageBucket: "classhub-a2bf2.firebasestorage.app",
+  messagingSenderId: "788231446286",
+  appId: "1:788231446286:web:c6c0a6e27380c00f92815f",
+  measurementId: "G-7RMH8QSGXR"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
